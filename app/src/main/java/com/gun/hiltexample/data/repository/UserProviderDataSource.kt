74a489dd107dbb8ecc572ae990109b67776e89f9
YoungGeon, Moon <my_gun5389@naver.com>
2023-03-15 19:27:57 +0900

    [Room] - Room DB 사용 시  DAO 메서드에 suspend 키워드로 인해 별도의 코루틴을 생성하지 않도록 수정 - DAO 메서드에 suspend를 붙이면 Room은 자체 디스패처를 사용하여 백그라운드 스레드에서 쿼리를 실행한다.

--- app/src/main/java/com/gun/hiltexample/data/repository/UserProviderDataSource.kt
+++ app/src/main/java/com/gun/hiltexample/data/repository/UserProviderDataSource.kt
@@ -7,9 +7,6 @@ import android.content.ContentValues
 import android.util.Log
 import com.gun.hiltexample.constant.Constants
 import com.gun.hiltexample.data.dto.User
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class UserProviderDataSource @Inject constructor(
@@ -17,145 +14,136 @@ class UserProviderDataSource @Inject constructor(
 ) : UserDataSource {
     @SuppressLint("Range")
     override suspend fun getUserList(): MutableList<User> {
-        return withContext(CoroutineScope(Dispatchers.IO).coroutineContext) {
-            val cursor = contentResolver.query(
-                Constants.URI_EXAMPLE,
-                arrayOf(AppDatabase.COL_USER_ID, AppDatabase.COL_USER_NAME), null, null,
-                AppDatabase.COL_USER_ID
-            )
+        val cursor = contentResolver.query(
+            Constants.URI_EXAMPLE,
+            arrayOf(AppDatabase.COL_USER_ID, AppDatabase.COL_USER_NAME), null, null,
+            AppDatabase.COL_USER_ID
+        )
 
-            val userList = arrayListOf<User>()
+        val userList = arrayListOf<User>()
 
-            cursor?.let {
-                while (cursor.moveToNext()) {
-                    val id = cursor.getInt(cursor.getColumnIndex(AppDatabase.COL_USER_ID))
-                    val name = cursor.getString(cursor.getColumnIndex(AppDatabase.COL_USER_NAME))
-                    userList.add(User(id, name))
-                }
-
-                cursor.close()
+        cursor?.let {
+            while (cursor.moveToNext()) {
+                val id = cursor.getInt(cursor.getColumnIndex(AppDatabase.COL_USER_ID))
+                val name = cursor.getString(cursor.getColumnIndex(AppDatabase.COL_USER_NAME))
+                userList.add(User(id, name))
             }
 
-            Log.d(Constants.TAG, "UserProviderDataSource - getUserList() - userList : $userList")
-
-            userList
+            cursor.close()
         }
+
+        Log.d(Constants.TAG, "UserProviderDataSource - getUserList() - userList : $userList")
+
+        return userList
     }
 
     @SuppressLint("Range")
     override suspend fun getUserListByIds(vararg userIds: Int): MutableList<User> {
-        return withContext(CoroutineScope(Dispatchers.IO).coroutineContext) {
-            val cursor = contentResolver.query(
-                Constants.URI_EXAMPLE,
-                arrayOf(AppDatabase.COL_USER_ID, AppDatabase.COL_USER_NAME),
-                AppDatabase.COL_USER_ID,
-                userIds.map { it.toString() }.toTypedArray(),
-                AppDatabase.COL_USER_ID
-            )
+        val cursor = contentResolver.query(
+            Constants.URI_EXAMPLE,
+            arrayOf(AppDatabase.COL_USER_ID, AppDatabase.COL_USER_NAME),
+            AppDatabase.COL_USER_ID,
+            userIds.map { it.toString() }.toTypedArray(),
+            AppDatabase.COL_USER_ID
+        )
 
-            val userList = arrayListOf<User>()
+        val userList = arrayListOf<User>()
 
-            cursor?.let {
-                while (cursor.moveToNext()) {
-                    val id = cursor.getInt(cursor.getColumnIndex(AppDatabase.COL_USER_ID))
-                    val name = cursor.getString(cursor.getColumnIndex(AppDatabase.COL_USER_NAME))
-                    userList.add(User(id, name))
-                }
-
-                cursor.close()
+        cursor?.let {
+            while (cursor.moveToNext()) {
+                val id = cursor.getInt(cursor.getColumnIndex(AppDatabase.COL_USER_ID))
+                val name = cursor.getString(cursor.getColumnIndex(AppDatabase.COL_USER_NAME))
+                userList.add(User(id, name))
             }
 
-            Log.d(Constants.TAG, "UserProviderDataSource - getUserListByIds() - userIds : $userIds, userList : $userList")
-
-            userList
+            cursor.close()
         }
+
+        Log.d(
+            Constants.TAG,
+            "UserProviderDataSource - getUserListByIds() - userIds : $userIds, userList : $userList"
+        )
+
+        return userList
     }
 
     @SuppressLint("Range")
     override suspend fun getUserByName(name: String): User? {
-        return withContext(CoroutineScope(Dispatchers.IO).coroutineContext) {
-            val cursor = contentResolver.query(
-                Constants.URI_EXAMPLE,
-                arrayOf(AppDatabase.COL_USER_NAME),
-                AppDatabase.COL_USER_ID,
-                arrayOf(name),
-                AppDatabase.COL_USER_ID
-            )
+        val cursor = contentResolver.query(
+            Constants.URI_EXAMPLE,
+            arrayOf(AppDatabase.COL_USER_NAME),
+            AppDatabase.COL_USER_ID,
+            arrayOf(name),
+            AppDatabase.COL_USER_ID
+        )
 
-            var user: User? = null
+        var user: User? = null
 
-            cursor?.let {
-                if (cursor.moveToFirst()) {
-                    val id = cursor.getInt(cursor.getColumnIndex(AppDatabase.COL_USER_ID))
-                    val name = cursor.getString(cursor.getColumnIndex(AppDatabase.COL_USER_NAME))
-                    user = User(id, name)
-                }
-                cursor.close()
+        cursor?.let {
+            if (cursor.moveToFirst()) {
+                val id = cursor.getInt(cursor.getColumnIndex(AppDatabase.COL_USER_ID))
+                val name = cursor.getString(cursor.getColumnIndex(AppDatabase.COL_USER_NAME))
+                user = User(id, name)
             }
-
-            Log.d(Constants.TAG, "UserProviderDataSource - getUserByName() - user : $user")
-
-            user
+            cursor.close()
         }
+
+        Log.d(Constants.TAG, "UserProviderDataSource - getUserByName() - user : $user")
+
+        return user
     }
 
     override suspend fun insertUsers(user: User): Long {
-        return withContext(CoroutineScope(Dispatchers.IO).coroutineContext) {
-            val rowValue = ContentValues()
-            rowValue.put(AppDatabase.COL_USER_NAME, user.name)
+        val rowValue = ContentValues()
+        rowValue.put(AppDatabase.COL_USER_NAME, user.name)
 
-            val uri = contentResolver.insert(
-                Constants.URI_EXAMPLE,
-                rowValue
-            )
+        val uri = contentResolver.insert(
+            Constants.URI_EXAMPLE,
+            rowValue
+        )
 
-            val result = uri?.let {
-                contentResolver.notifyChange(it, null)
-                ContentUris.parseId(it)
-            } ?: -1
+        val result = uri?.let {
+            contentResolver.notifyChange(it, null)
+            ContentUris.parseId(it)
+        } ?: -1
 
-            Log.d(Constants.TAG, "UserProviderDataSource - insertUsers() - result : $result")
+        Log.d(Constants.TAG, "UserProviderDataSource - insertUsers() - result : $result")
 
-            return@withContext result
-        }
+        return result
     }
 
     override suspend fun updateUser(user: User): Int {
-        return withContext(CoroutineScope(Dispatchers.IO).coroutineContext) {
-            val updateValues = ContentValues().apply {
-                put(AppDatabase.COL_USER_NAME, user.name)
-            }
-
-            val selectionClause: String = AppDatabase.COL_USER_ID + "LIKE ?"
-            val selectionArgs: Array<String> = arrayOf(user.userId.toString())
-
-            val result: Int = contentResolver.update(
-                Constants.URI_EXAMPLE,
-                updateValues,
-                selectionClause,
-                selectionArgs
-            )
-
-            Log.d(Constants.TAG, "UserProviderDataSource - updateUser() - result : $result")
-
-            return@withContext result
+        val updateValues = ContentValues().apply {
+            put(AppDatabase.COL_USER_NAME, user.name)
         }
+
+        val selectionClause: String = AppDatabase.COL_USER_ID + "LIKE ?"
+        val selectionArgs: Array<String> = arrayOf(user.userId.toString())
+
+        val result: Int = contentResolver.update(
+            Constants.URI_EXAMPLE,
+            updateValues,
+            selectionClause,
+            selectionArgs
+        )
+
+        Log.d(Constants.TAG, "UserProviderDataSource - updateUser() - result : $result")
+
+        return result
     }
 
     override suspend fun deleteUser(user: User): Int {
-        return withContext(CoroutineScope(Dispatchers.IO).coroutineContext) {
-            val selectionClause: String = AppDatabase.COL_USER_ID + "LIKE ?"
-            val selectionArgs: Array<String> = arrayOf(user.userId.toString())
+        val selectionClause: String = AppDatabase.COL_USER_ID + "LIKE ?"
+        val selectionArgs: Array<String> = arrayOf(user.userId.toString())
 
-            val result: Int = contentResolver.delete(
-                Constants.URI_EXAMPLE,
-                selectionClause,
-                selectionArgs
-            )
+        val result: Int = contentResolver.delete(
+            Constants.URI_EXAMPLE,
+            selectionClause,
+            selectionArgs
+        )
 
-            Log.d(Constants.TAG, "UserProviderDataSource - updateUser() - result : $result")
+        Log.d(Constants.TAG, "UserProviderDataSource - updateUser() - result : $result")
 
-            return@withContext result
-        }
+        return result
     }
 }
